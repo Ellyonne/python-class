2dd60a4c21177c70f7d01b32f81e26f2d2ae78b5
suleiman rasheed <ellyonne@gmail.com>
2023-07-25 16:50:18 +0100

    e ing

--- assesment.py
+++ assesment.py
@@ -14,5 +14,6 @@
 random_stuff = ['hello', '23', '3.4','night', 'welcome']
 
 def get_on_string_with_characters(lst):
+   
     return[value for value in lst if value.isalpha()]
 print(get_on_string_with_characters(random_stuff))
